bc3186e5a6fc8268e427d8cc711d49c002afafd7
DionneMaguire <dionnemaguire@yahoo.com>
2022-10-17 14:48:12 +0000

    initial commit

---
+++ dionnes_wines/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dionnes_wines project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dionnes_wines.settings')
+
+application = get_asgi_application()
