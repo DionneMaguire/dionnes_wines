1facb7a0da469896807855b40652072705f1cd32
DionneMaguire <dionnemaguire@yahoo.com>
2022-10-28 17:46:53 +0000

    added checkout views and templates

--- checkout/admin.py
+++ checkout/admin.py
@@ -16,14 +16,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'street_address1',
-              'street_address2', 'town_or_city', 
+              'street_address2', 'town_or_city',
               'county', 'country', 'postcode', 'delivery_cost',
               'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
                     'grand_total',)
- 
+
     ordering = ('-date',)
+
 
 admin.site.register(Order, OrderAdmin)
