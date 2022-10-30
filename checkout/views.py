345bcff20f513b74d35b1749f8de6c53c2fb912e
DionneMaguire <dionnemaguire@yahoo.com>
2022-10-30 16:29:08 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LifPjAU6MTgGD6NlRpMVCi7rL9ff27QCOS03TiWXM9vs1SXu85QcCk3QjfrSh9VOaUWxF3EFXeJeM5gLU1C9JsI00aPfg6Mck',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
