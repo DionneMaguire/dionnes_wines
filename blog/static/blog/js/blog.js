1765eaf8cd8c804f7a15bf08148164ae3dbaffc8
DionneMaguire <dionnemaguire@yahoo.com>
2022-11-15 20:18:03 +0000

    added js for delete confirmation on the blog list and detail page

---
+++ blog/static/blog/js/blog.js
@@ -0,0 +1,11 @@
+    // handles back to top button
+    $('.btt-link').click(function (e) {
+        window.scrollTo(0, 0)
+    })
+    // confirmation before delete
+    $('.delete_button').click(function(e){
+        var result = confirm("Are you sure you want to delete this blog?");
+        if(!result) {
+            e.preventDefault();
+        }
+    });
